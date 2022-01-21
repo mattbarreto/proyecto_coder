9e3a804a15d0ebec4812db242a8be2135820c7d9
Felix Barreto <matiasbarreto@gmail.com>
2022-01-21 17:48:17 -0300

    str en todas las clases de models-py
    Se crearon las ramas trainer y training.

--- app_proyectoFinal/models.py
+++ app_proyectoFinal/models.py
@@ -3,7 +3,6 @@ from django.db.models.fields import CharField, IntegerField, EmailField, DateFie
 
 # Create your models here.
 
-
 class Atleta(Model):
 
     nombre = CharField(max_length=40)
@@ -15,17 +14,18 @@ class Atleta(Model):
     def __str__(self):
         return f'Nombre y apellido: {self.nombre} {self.apellido} Edad: {self.edad} Altura: {self.altura} Peso: {self.peso} Email: {self.email}'
 
-
-
 class Entrenador(Model):
 
     nombre = CharField(max_length=40)
     apellido = CharField(max_length=40)
     email = EmailField()
     profesion = CharField(max_length=50)
-
+    def __str__(self):
+        return f'Nombre y apellido: {self.nombre} {self.apellido} Profesion: {self.profesion} Email: {self.email}'
 
 class Rutina(Model):
 
     nombre = CharField(max_length=40)
     fecha_inicio = DateField()
+    def __str__(self):
+        return f'Rutina: {self.nombre} Fecha de inicio: {self.fecha_inicio}'
