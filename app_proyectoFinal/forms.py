8d4b17eb3f97fc5f431ee687e76d3b8f4a0f3021
Felix Barreto <matiasbarreto@gmail.com>
2022-01-26 12:06:56 -0300

    Se agrego carpeta .gitignore.

--- app_proyectoFinal/forms.py
+++ app_proyectoFinal/forms.py
@@ -1,4 +1,4 @@
-from django.forms import Form, IntegerField, CharField, EmailField, FloatField
+from django.forms import DateField, Form, IntegerField, CharField, EmailField, FloatField
 
 class atleta_create(Form):
     nombre = CharField()
@@ -7,3 +7,19 @@ class atleta_create(Form):
     altura = FloatField()
     peso = FloatField()
     email = EmailField()
+    
+class rutinas_create(Form):
+
+    nombre = CharField()
+    fecha_inicio = DateField()
+    intensidad = CharField()
+    ejercicio_1 = CharField()
+    ejercicio_2 = CharField()
+    ejercicio_3 = CharField()
+    ejercicio_4 = CharField()
+    ejercicio_5 = CharField()
+    ejercicio_6 = CharField()
+    ejercicio_7 = CharField()
+    duracionPorEjercicio = IntegerField()
+    descansoEntreEjercicio = IntegerField()
+    rondas = IntegerField()
